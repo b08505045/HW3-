e7c79c3b33777b6d69e3a6139cc43391d358ecea
b08505045 <84371904+b08505045@users.noreply.github.com>
2022-05-03 16:15:52 +0800

    Update 3-4.c

--- 3-5/3-4.c
+++ 3-5/3-4.c
@@ -4,42 +4,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#define D 94 // number of total inputs
-#define Q 7393913 // mod q
 
 long long mod(long long t, long long q){
-    if(t >= 0)
+    if(t > 0)
         return t % q;
+    else if(t == 0) return 0;
     else
         return t % q + q;
 }
 
 int main(){
-    char *c = malloc(10000000 * sizeof(char));
-    scanf("%s", c);
-    int length = strlen(c);
-    char *s = malloc((length + 1) * sizeof(char));
-    strncpy(s, c, length); // s will be the input
-    s[length] = '\0';
-    free(c);
+    char *s = malloc(10000000 * sizeof(char));
+    scanf("%s", s);
+    int length = strlen(s);
     
-    long long t, p, h, temp; // t = string comapre to pattern, p = pattern
-    int tf = length - 1, tl = 1; // index t begin, which will be the last (or first) index of palindrome
-    p = mod(s[0] - '!', Q);
-    t = p;
+    unsigned long long D = 94, Q = 18014398777917439, H = D % Q;
+    long long p = mod(s[0] - '!', Q), t = p, h = 1, temp; // t = string comapre to pattern, p = pattern
+    int tf = length - 1, tl; // index t begin, which will be the last (or first) index of palindrome
     int j = 0, k = length - 1; // last (or first) index of first and last p
-    int m = 0;
+    int m = 0, M = 0;
 
     // find first palindrome
+    int same = 1;
     while(1){
         // find first index of tf
         for(tf; tf >= 0; tf--){
-            if(s[tf] == s[0]) break;
+            if(s[tf] == s[0]) break;        
         }
         // printf("tf now : %d\n", tf);
         int i = tf;
         // Let's find !
-        while(i != j && i != j + 1){
+        while(i > j + 2){
             temp = mod(D * t + (s[i - 1] - '!'), Q);
             // if current t = p
             if(temp == mod(D * p + (s[j + 1] - '!'), Q) && s[i - 1] == s[j + 1]){
@@ -51,35 +46,34 @@ int main(){
             }
             // if t != p
             else{
+                same = 0;
                 // printf("p : %lld\n", p);
                 // printf("i now : %d\n", i);
                 // find new start index of t that t = p
                 int repeat = 0;
-                while(1){
+                while(tf != j){
                     if(!repeat){
-                        if(m <= 9)
-                            h = mod(pow(D, m), Q);
-                        else{
-                            h = mod(D, Q);
-                            for(int a = 1; a < m; a++)
-                                h = mod(h, mod(D, Q)); // (A * B) mod C = (A mod C * B mod C) mod C
-                        }
+                        int dif = m - M;
+                        for(int a = 0; a < dif; a++)
+                            h = mod(h * H, Q); // (A * B) mod C = (A mod C * B mod C) mod C
+                        M = m;
                     }
-                    for(i; i > j;){
-                        t = mod(D * (t - (s[tf] - '!') * h) + s[--i] - '!', Q);
+                    for(tf; tf > j;){
+                        t = mod(D * (t - (s[tf] - '!') * h) + (s[--i] - '!'), Q);
                         tf--;
                         if(s[tf] == s[0])
                             break;
                     }
                     // printf("tf : %d, i : %d\n", tf, i);
                     // printf("t : %lld\n", t);
-                    if(i == j) break;
-                    else if(t == p){
+                    if(t == p){
                         // printf("Let's compare\n");
+                        same = 1;
                         repeat = 0;
                         // if t = p, check character one by one
                         for(int a = 0; a <= tf - i; a++){
                             if(s[a] != s[tf - a]){
+                                same = 0;
                                 repeat = 1;
                                 break;
                             }
@@ -93,29 +87,22 @@ int main(){
                 }
             }
         }
-        int same = 0;
         // i and j encounter
-        if(t == p){
-            same = 1;
-            // if t = p, check character one by one
-            for(int a = 0; a <= j; a++){
-                if(s[a] != s[tf - a]){
-                    same = 0;
-                    break;
-                }
-            }
-        }
         if(same) break;
         else{
-            p = s[0] - '!';
+            same = 1;
+            p = mod(s[0] - '!', Q);
             t = p;
+            h = 1;
             m = 0;
+            M = 0;
             j = 0;
-            tf = i;
+            tf--;
+            // printf("tf : %d\n", tf);
         }
     }
     //------------------------------------------------------------printf first palindrome
-    // printf("%lld\n", tf);
+    // printf("%lld\n", tf + 1);
     // for(int i = 0; i <= tf; i++){
     //     printf("%c", s[i]);
     // }
@@ -127,9 +114,16 @@ int main(){
         printf("%d\n%s\n", 0, s);
     // OK FINE
     else{
-        p = s[length - 1] - '!'; // p now start from s's last
+        same = 1;
+        // printf("Fine\n-------------------------------------\n");
+        p = mod(s[length - 1] - '!', Q); // p now start from s's last
         t = p;
+        h = 1;
         m = 0;
+        M = 0;
+        k = length - 1;
+        tl = 1;
+        // printf("init t : %lld, init p : %lld\n", t, p);
         while(1){
             // find first index of tl
             for(tl; tl < length; tl++){
@@ -137,7 +131,7 @@ int main(){
             }
             int i = tl;
             // Let's find !
-            while(i != k && i != k - 1){ 
+            while(i < k - 2){ 
                 temp = mod(D * t + (s[i + 1] - '!'), Q);
                 // if current t = p
                 if(temp == mod(D * p + (s[k - 1] - '!'), Q) && s[i + 1] == s[k - 1]){
@@ -149,30 +143,31 @@ int main(){
                 }
                 // if t != p
                 else{
+                    same = 0;
+                    // printf("t != p\n");
                     int repeat = 0;
                     // find new start index of t that t = p
-                    while(1){
+                    while(tl != k){
                         if(!repeat){
-                            if(m <= 9)
-                                h = mod(pow(D, m), Q);
-                            else{
-                                h = mod(D, Q);
-                                for(int a = 1; a < m; a++)
-                                    h = mod(h, mod(D, Q)); // (A * B) mod C = (A mod C * B mod C) mod C
-                            }
+                            int dif = m - M;
+                            for(int a = 0; a < dif; a++)
+                                h = mod(h * H, Q); // (A * B) mod C = (A mod C * B mod C) mod C
+                            M = m;
                         }
-                        for(i; i < k;){
+                        for(tl; tl < k;){
                             t = mod(D * (t - (s[tl] - '!') * h) + (s[++i] - '!'), Q);
                             tl++;
                             if(s[tl] == s[length - 1])
                                 break;
                         }
-                        if(i == k) break;
-                        else if(t == p){
+                        if(t == p){
+                            same = 1;
                             // printf("Let's compare\n");
                             repeat = 0;
+                            // printf("break?\n");
                             for(int a = 0; a <= i - tl; a++){
                                 if(s[tl + a] != s[length - 1 - a]){
+                                    same = 0;
                                     repeat = 1;
                                     break;
                                 }
@@ -186,29 +181,23 @@ int main(){
                     }
                 }
             }
-            int same = 0;
-            // i and j encounter
-            if(t == p){
-                same = 1;
-                for(int a = 0; a <= length - k - 1; a++){
-                    if(s[tl + a] != s[length - 1 - a]){
-                        same = 0;
-                        break;
-                    }
-                }
-            }
             if(same) break;
             else{
-                p = s[length - 1] - '!';
-                t = p;
+                same = 1;
+                p = mod(s[length - 1] - '!', Q);
+                t = p; 
+                h = 1;
                 m = 0;
-                k = 0;
-                tl = i;
+                M = 0;
+                k = length - 1;
+                tl++;
             }
         }
-        printf("Let's Start!\n");
+        // printf("tf : %d, tl : %d\n", tf, tl);
         int dis_f = tf + 1, dis_l = length - tl;
+        // printf("dis_f : %d, dis_l : %d\n", dis_f, dis_l);
         if(dis_f > dis_l){
+            // printf("f > l\n");
             int add_length = length - dis_f;
             printf("%d\n", add_length);
             char *add = malloc((add_length + 1) * sizeof(char));
